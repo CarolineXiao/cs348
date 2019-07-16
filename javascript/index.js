519f60ac20daa269367f6915cabf2956e93e0345
Spinspincoupling <wzq145@gmail.com>
2019-07-16 15:28:18 -0400

    commit for connection test

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,16 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
+$(document).ready(() => {
+    document.getElementById('date').innerHTML = new Date().toDateString();
+
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
+    });
+
+    con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    });
+})
