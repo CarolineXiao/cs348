208c94faa3ae9dfea5c0a0684a5a2bc834f009d3
Spinspincoupling <wzq145@gmail.com>
2019-07-18 19:26:32 -0400

    ajax call test

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,25 @@
 $(document).ready(() => {
     document.getElementById('date').innerHTML = new Date().toDateString();
+    const dateObj = new Date();
+    const month = dateObj.getUTCMonth() + 1; //months from 1-12
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const newdate = year + "-" + month + "-" + day;
 
-    var mysql = require('mysql');
+    $.get(`http://localhost:8081/menu`, function(data, status){
+        console.log(data);
+        console.log(status);
+      });
 
-    var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
-    });
+      $.post(`http://localhost:8081/menu/insert`,
+      {
+        firstName: "Donald",
+        lastName: "Duck",
+        workDept: "Fin",
+        hireDate: newdate
+      },
+      function(data, status){
+        console.log(data);
+      });
 
-    con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    });
 })
